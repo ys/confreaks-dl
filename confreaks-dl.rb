eb30bae19c4862e53d8ab6bdaaa36b9a78d59766
Yannick Schutz <yannick.schutz@gmail.com>
2012-11-07 20:31:24 +0100

    Download to Movies folder and correct name

--- confreaks-dl.rb
+++ confreaks-dl.rb
@@ -2,10 +2,11 @@ require 'open-uri'
 require 'nokogiri'
 conf_name = ARGV[0]
 doc = Nokogiri::HTML(open("http://confreaks.com/events/#{conf_name}"))
+`mkdir -p  #{ENV['HOME']}/Movies/screencasts/confs/`
 doc.css('a').select { |a| a['href'] =~ /videos/ }.map{ |a| a['href'] }.uniq.each do |link|
   link = "http://confreaks.com#{link}"
    doc_vid = Nokogiri::HTML(open(link))
    doc_vid.css('a').select { |a| a['href'] =~ /large/ }.map{ |a| a['href']}.each do |link|
-     system "wget -v #{link}"
+     `wget -O #{ENV['HOME']}/Movies/screencasts/confs/#{/(.*)\/(.*)\?(.*)/.match(link)[2]} -v #{link}`
    end
 end
